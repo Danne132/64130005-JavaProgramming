e763cf9b104c28b65582fa8476184662029ee121
Danne132 <an.hd.64cntt@ntu.edu.vn>
2024-10-01 14:06:15 +0700

    Update bài

--- BT_Dictionary/src/MainApp.java
+++ BT_Dictionary/src/MainApp.java
@@ -1,9 +1,26 @@
+import java.util.HashMap;
+import java.util.Scanner;
 
 public class MainApp {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		HashMap<String, String> dicHashMap = new HashMap<String, String>();
+		dicHashMap.put("hello", "xin chào");
+        dicHashMap.put("goodbye", "tạm biệt");
+        dicHashMap.put("thank you", "cảm ơn");
+        dicHashMap.put("please", "làm ơn");
+        dicHashMap.put("sorry", "xin lỗi");
+        dicHashMap.put("yes", "vâng");
+        dicHashMap.put("house", "nhà");
+        dicHashMap.put("friend", "bạn bè");
+        dicHashMap.put("hungry", "đói");
+        dicHashMap.put("full", "đầy đủ");
+		
+        String translated_word = "hello";
+        
+        if(dicHashMap.containsKey(translated_word)) {
+        	System.out.println("Dịch: " + dicHashMap.get(translated_word));
+        }
 	}
 
 }
