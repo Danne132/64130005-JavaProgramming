0c6e4403c0dd291e9811554fba2023279d0f22ad
Danne132 <an.hd.64cntt@ntu.edu.vn>
2024-10-01 13:33:22 +0700

    Xóa file dư

--- BT_HashMap/src/Vidu/EmployeeData.java
+++ BT_HashMap/src/Vidu/EmployeeData.java
@@ -3,12 +3,16 @@ package Vidu;
 import java.util.jar.Attributes.Name;
 
 public class EmployeeData {
-	String name;
+	String namee;
 	int salary;
 	
 	public EmployeeData(String name) {
-		this.name = name;
+		this.namee = name;
 		salary = 5600;
 	}
-	
+
+	@Override
+	public String toString() {
+		return "EmployeeData [name=" + namee + ", salary=" + salary + "]";
+	}
 }
--- BT_HashMap/src/Vidu/Main.java
+++ BT_HashMap/src/Vidu/Main.java
@@ -1,9 +1,14 @@
 package Vidu;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Main {
 
 	public static void main(String[] args) {
-		
+		Map<String, EmployeeData> staffObj = new HashMap<String, EmployeeData>();
+		staffObj.put("101", new EmployeeData("An"));
+		staffObj.put("102", new EmployeeData("Haha"));
 	}
 
 }
