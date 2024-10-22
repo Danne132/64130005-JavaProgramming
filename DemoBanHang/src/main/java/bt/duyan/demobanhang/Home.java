caf834572bd2b6ac523551c0686b3861b448531a
Danne132 <an.hd.64cntt@ntu.edu.vn>
2024-10-22 16:40:05 +0700

    up lần 3 kết nối CSDL

--- DemoBanHang/src/main/java/bt/duyan/demobanhang/Home.java
+++ DemoBanHang/src/main/java/bt/duyan/demobanhang/Home.java
@@ -1,8 +1,6 @@
 package bt.duyan.demobanhang;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
+import java.sql.*;
 
 public class Home {
     public static void main(String[] args) throws SQLException, ClassNotFoundException {
@@ -10,7 +8,23 @@ public class Home {
         // Mở kết nối
         // 2.1 Chuỗi két nối
         String strCont = "jdbc:mysql://localhost:3306/demo";
+
+        // 2.2 Mở kết nối
         Connection conn = DriverManager.getConnection(strCont,"root","");
-        System.out.printf("Haha");
+//        System.out.printf("Haha");
+
+        // Select
+        String sqlSelect = "select * from sanpham";
+        Statement lenh  = conn.createStatement();
+        ResultSet ketQua = lenh.executeQuery(sqlSelect);
+
+        //Hiện kết quả
+        while (ketQua.next()){
+            int id = ketQua.getInt(1); //Lấy dữ liệu từ cột 0
+            String tenSP = ketQua.getString(2);
+            String giaSP = ketQua.getString(3);
+            String moTa = ketQua.getString(4);
+            System.out.println(id+ " | " + tenSP + " | " + giaSP + " | " + moTa);
+        }
     }
 }
