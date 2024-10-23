d3d6069322225f783d87b03547c33ebdb28ca8bf
Danne132 <an.hd.64cntt@ntu.edu.vn>
2024-10-23 22:44:36 +0700

    Xóa project

--- demoSQL_tableView/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module bt.duyan.demosql_tableview {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens bt.duyan.demosql_tableview to javafx.fxml;
-    exports bt.duyan.demosql_tableview;
-}
