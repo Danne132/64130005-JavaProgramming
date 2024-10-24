9a4d09847060e8ae4db1fc33cc32de7ce446ecce
Danne132 <an.hd.64cntt@ntu.edu.vn>
2024-10-24 21:36:03 +0700

    Lấy dữ liệu(1)

--- demoSQL/src/main/java/bt/duyan/demosql/demoSQLController.java
+++ demoSQL/src/main/java/bt/duyan/demosql/demoSQLController.java
@@ -1,28 +1,51 @@
 package bt.duyan.demosql;
 
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
+import javafx.scene.control.Tab;
+import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
+import java.sql.*;
 
 public class demoSQLController {
     @FXML
     TableView<SanPham> tableView;
+    //Tạo ra các trường tương ứng
+    TableColumn<SanPham, Integer> id;
+    TableColumn<SanPham, String> tenSP;
+    TableColumn<SanPham, Double> donGia;
+    TableColumn<SanPham, String> moTa;
+    //Cập nhật giao diện khi dữ liệu thay đổi
+    private ObservableList<SanPham> data;
+    Connection conn;
 
     // Kết nối database
     void connectDTB(){
         //Nhập vào đường dẫn của database
         String url = "jdbc:mysql://localhost:3306/demo";
         try {
-            Connection conn = DriverManager.getConnection(url, "root", "");
+            conn = DriverManager.getConnection(url, "root", "");
             System.out.println("Kết nối thành công");
         } catch (SQLException e) {
             System.out.println("Kết nối thất bại");
         }
     }
 
+    void getData(){
+        //Lấy dữ liệu từ bảng sanpham trong database
+        String sqlSelect = "select * from sanpham";
+        Statement lenh  = null;
+        try {
+            lenh = conn.createStatement();
+            ResultSet ketQua = lenh.executeQuery(sqlSelect);
+            while(ketQua.next()) {
+            }
 
+        } catch (SQLException e) {
+            System.out.println("Lỗi");
+        }
+
+    }
 
 }
