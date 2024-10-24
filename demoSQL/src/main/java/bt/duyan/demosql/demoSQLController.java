70d2f7cf9e3ea5e55cbbec81bf108fbb92effb8d
Danne132 <an.hd.64cntt@ntu.edu.vn>
2024-10-24 21:03:07 +0700

    kết nối Database

--- demoSQL/src/main/java/bt/duyan/demosql/demoSQLController.java
+++ demoSQL/src/main/java/bt/duyan/demosql/demoSQLController.java
@@ -3,7 +3,26 @@ package bt.duyan.demosql;
 import javafx.fxml.FXML;
 import javafx.scene.control.TableView;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class demoSQLController {
     @FXML
-    TableView tableView;
+    TableView<SanPham> tableView;
+
+    // Kết nối database
+    void connectDTB(){
+        //Nhập vào đường dẫn của database
+        String url = "jdbc:mysql://localhost:3306/demo";
+        try {
+            Connection conn = DriverManager.getConnection(url, "root", "");
+            System.out.println("Kết nối thành công");
+        } catch (SQLException e) {
+            System.out.println("Kết nối thất bại");
+        }
+    }
+
+
+
 }
--- demoSQL/src/main/java/module-info.java
+++ demoSQL/src/main/java/module-info.java
@@ -1,6 +1,7 @@
 module bt.duyan.demosql {
     requires javafx.controls;
     requires javafx.fxml;
+    requires java.sql;
 
 
     opens bt.duyan.demosql to javafx.fxml;
