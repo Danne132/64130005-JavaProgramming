71e6cce7d2f5b572cc6e6bc4eaa0484551af575e
Danne132 <an.hd.64cntt@ntu.edu.vn>
2024-11-15 12:24:13 +0700

    Bài cũ

--- DemoBanHang/src/main/java/bt/duyan/demobanhang/HelloApplication.java
+++ DemoBanHang/src/main/java/bt/duyan/demobanhang/HelloApplication.java
@@ -6,20 +6,42 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.sql.*;
 
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        Scene scene = new Scene(fxmlLoader.load());
         stage.setTitle("Hello!");
         stage.setScene(scene);
         stage.show();
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws ClassNotFoundException, SQLException {
         launch();
         // Đăng ký diver
+        Class.forName("com.mysql.cj.jdbc.Driver");
+        // Mở kết nối
+        // 2.1 Chuỗi két nối
+        String strCont = "jdbc:mysql://localhost:3306/demo";
 
+        // 2.2 Mở kết nối
+        Connection conn = DriverManager.getConnection(strCont,"root","");
+//        System.out.printf("Haha");
+
+        // Select
+        String sqlSelect = "select * from sanpham";
+        Statement lenh  = conn.createStatement();
+        ResultSet ketQua = lenh.executeQuery(sqlSelect);
+
+        //Hiện kết quả
+        while (ketQua.next()){
+            int id = ketQua.getInt(1); //Lấy dữ liệu từ cột 0
+            String tenSP = ketQua.getString(2);
+            String giaSP = ketQua.getString(3);
+            String moTa = ketQua.getString(4);
+            System.out.println(id+ " | " + tenSP + " | " + giaSP + " | " + moTa);
+        }
     }
 }
